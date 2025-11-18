400195fa27b0f529a7f66cc7ad70751abf8ea868
Gerardo Iglesias Becerril <gerardoib@outlook.com>
2025-11-18 11:41:59 -0600

    Debug changes on create product

--- back/middelwares/cors.js
+++ back/middelwares/cors.js
@@ -23,7 +23,7 @@ const corsOptions = {
         return callback(new Error(msg), false);
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
 
